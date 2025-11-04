af3e7856c31965378f7ecb79f884969634b15947
Lutfi <lutfihakim288@gmail.com>
2025-11-04 21:10:22 +0700

    feat(transaction): add Select Category page

---
+++ lib/features/transaction/presentation/pages/select_category_page.dart
@@ -0,0 +1,39 @@
+import 'package:dompet/core/enum/category.dart';
+import 'package:dompet/core/widgets/category_selector.dart';
+import 'package:dompet/theme_data.dart';
+import 'package:flutter/material.dart';
+
+class SelectCategoryPage extends StatelessWidget {
+  final String? selectedCategoryIconKey;
+
+  const SelectCategoryPage({
+    super.key,
+    this.selectedCategoryIconKey,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final Category? selectedCategory = selectedCategoryIconKey != null
+        ? Category.fromValue(selectedCategoryIconKey!)
+        : null;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select Category'),
+        backgroundColor: AppTheme.backgroundColor,
+        foregroundColor: AppTheme.textColorPrimary,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: CategorySelector(
+          selectedCategory: selectedCategory,
+          onSelected: (category) {
+            // Return the selected category back to the calling screen
+            Navigator.of(context).pop(category);
+          },
+        ),
+      ),
+    );
+  }
+}
